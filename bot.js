bb0d3977af88a62d2607efbfe1e3ea2fbf358192
Uday <110768804+UdayScripts@users.noreply.github.com>
2024-06-26 20:11:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ const {
 //Activating .env file
 
 //Creating intance of our bot and export to other file
-module.exports = new Telegraf('6368202444:AAFrL3C45CJKtnsfYNfMgullsMAsSlhnEZ4')
+module.exports = new Telegraf('6644986626:AAE8OWg5jO-qao2EuDMuO957tF4fwjA8jrs')
